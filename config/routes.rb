10cdf125071fd8b52e3b963dc12b78da9f8e231b
nabe <197488176+nabe-jp@users.noreply.github.com>
2025-03-22 06:25:14 +0000

    Revert "Bookers2_app-iine_come_app_At4_25/03/22"
    This reverts commit 072d11099ebb4e494fa57888896d03da5dc1d255.

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,10 @@ Rails.application.routes.draw do
   get  'home/about', to: 'homes#about', as: 'about'
   devise_for :users
 
-  resources :books, only: [:index, :show, :edit, :create, :update, :destroy] do
+  resources :books, only: [:show, :index, :edit, :create, :update, :destroy] do
     resource :favorite, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
   
-  resources :users, only: [:index, :show, :edit, :update] do
-    resource :relationships, only: [:create, :destroy]
-    get 'followings' => 'relationships#followings', as: 'followings'
-    get 'followers' => 'relationships#followers', as: 'followers'
-  end
+  resources :users, only: [:show, :index, :edit, :update]
 end
